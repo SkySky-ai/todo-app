da05372dcaf77e584b0d8a45dcaa99de2afc18fb
Skylar <skylarlay2305@gmail.com>
2024-08-19 12:57:42 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,34 +1,26 @@
-def get_todos(filepath="todos.txt"):
-    """ Read a text file and return the list of
-    to-do items.
-    """
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
+# from functions import get_todos, write_todos
+import funtions
+import time
 
-
-def write_todos(todos_arg, filepath="todos.txt"):
-    """Write the to-do items list in the text file."""
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
-
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("It is", now)
 
 while True:
     user_action = input("Type add, show, edit, complete, or exit: ")
     user_action = user_action.strip()
 
-    if user_action.startswith('add'):
+    if user_action.startswith("add"):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = funtions.get_todos()
 
         todos.append(todo + '\n')
 
-        write_todos(todos)
+        funtions.write_todos(todos)
 
     elif user_action.startswith('show'):
 
-        todos = get_todos()
+        todos = funtions.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -40,12 +32,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos()
+            todos = funtions.get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            write_todos(todos)
+            funtions.write_todos(todos)
         except ValueError:
             print("Command does not exist")
             continue
@@ -54,12 +46,12 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = funtions.get_todos()
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos(todos)
+            funtions.write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list"
             print(message)
